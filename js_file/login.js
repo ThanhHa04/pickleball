6dadaf09fb7ebf6b5b0cc4bcb835194611adb822
Duc <buiminhduc22082008@gmail.com>
2025-02-24 09:09:40 +0700

    dang nhap voi Authentication Firebase

--- js_file/login.js
+++ js_file/login.js
@@ -19,4 +19,36 @@ function showLoginMessage(event) {
     }
 
     notification.style.display = 'block'; // Hiển thị thông báo
-} 
+}
+// Cấu hình Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyATp-eu8CBatLs04mHpZS4c66FaYw5zLgk",
+    authDomain: "pka-pickleball.firebaseapp.com",
+    projectId: "pka-pickleball",
+    storageBucket: "pka-pickleball.appspot.com",
+    messagingSenderId: "38130361867",
+    appId: "1:38130361867:web:f3c1a3940e3c390b11890e",
+    measurementId: "G-0YQ7GKJKRC"
+};
+
+// Khởi tạo Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+// Xử lý đăng nhập
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("login-form").addEventListener("submit", async function (e) {
+        e.preventDefault();
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        auth.signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                alert("Đăng nhập thành công!");
+                window.location.href = "/html_file/home.html"; // Chuyển hướng sau khi đăng nhập
+            })
+            .catch((error) => {
+                alert("Lỗi: " + error.message);
+            });
+    });
+});
