e1585396fba17573d3ed44506b468aba8b118bd5
Mekumine <119375233+ThanhHa04@users.noreply.github.com>
2025-03-21 20:53:21 +0700

    Delete js_file/config.js

--- js_file/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyATp-eu8CBatLs04mHpZS4c66FaYw5zLgk",
-    authDomain: "pka-pickleball.firebaseapp.com",   
-    projectId: "pka-pickleball",
-    storageBucket: "pka-pickleball.appspot.com",
-    messagingSenderId: "38130361867",
-    appId: "1:38130361867:web:f3c1a3940e3c390b11890e",
-    measurementId: "G-0YQ7GKJKRC"
-}
